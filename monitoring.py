4ec3f815c2f058eb23b998e1b19a000e2ecbec63
JAGoth <julian.gothe@fielmann.com>
2023-12-18 09:16:04 +0100

    edited the code based on pylint output

--- monitoring.py
+++ monitoring.py
@@ -1,29 +1,26 @@
 # Importing Libaries and local Files
-import sendhook
 import psutil
+import sendhook
 
 class GetMem():
 
     def __init__(self):
         self.mem = psutil.virtual_memory()
     
-    def getTotalMem(self, raw:bool=False):
+    def get_total_mem(self, raw:bool=False):
         total_mem = round(self.mem.total / 1024 ** 3, 2)
         if raw:
-            return str(total_mem) + " GB"
-        else:
             return total_mem
+        return str(total_mem) + " GB"
     
-    def getAvailableMem(self, raw:bool=False):
+    def get_available_mem(self, raw:bool=False):
         av_mem = round(self.mem.available / 1024 ** 3, 2)
         if raw:
-            return str(av_mem) + " GB"
-        else:
             return av_mem
+        return str(av_mem) + " GB"
     
-    def getUsedMem(self, raw:bool=False):
+    def get_used_mem(self, raw:bool=False):
         used_mem = round(self.mem.used / 1024 ** 3, 2)
         if raw:
-            return str(used_mem) + " GB"
-        else:
             return used_mem
+        return str(used_mem) + " GB"
